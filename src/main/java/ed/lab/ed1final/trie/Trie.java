c9b03d9d77e26696ac8687a10d27a475cb3107dc
Sophia <scoreap@correo.url.edu.gt>
2025-05-30 08:24:53 -0600

    intento

--- src/main/java/ed/lab/ed1final/trie/Trie.java
+++ src/main/java/ed/lab/ed1final/trie/Trie.java
@@ -1,25 +1,72 @@
 package ed.lab.ed1final.trie;
 
 import org.springframework.stereotype.Component;
+import org.w3c.dom.Node;
+
+import java.util.PriorityQueue;
+
 
 @Component
 public class Trie {
+
+    private Node root;
+
     public Trie() {
+        root = new Node();
     }
 
     public void insert(String word) {
-
+        Node temp = root;
+        for (char c : word.toCharArray()) {
+            if (temp.children[c - 'a'] == null) {
+                temp.children[c - 'a'] = new Node();
+            }
+            temp = temp.children[c - 'a'];
+        }
+        temp.isLast = true;
     }
 
     public int countWordsEqualTo(String word) {
-        return -1;
+        int result = 0;
+        int i = 0;
+        for (char c : word.toCharArray()) {
+            if (c == word.charAt(i)) {
+                result++;
+                i++;
+            } else {
+                break;
+            }
+        }
+        return result;
     }
 
     public int countWordsStartingWith(String prefix) {
-        return -1;
+        int result = 0;
+        Node current = root;
+            for(char c:prefix.toCharArray()){
+               if(current.children[c-'a'] == null)
+                    return 0;
+
+               current=current.children[c-'a'];
+               result++;
+            }
+        return result;
     }
 
     public void erase(String word) {
 
+
     }
+
+    //Crear Nodo
+    private static class Node {
+        public Node[] children;
+        public boolean isLast;
+
+        public Node() {
+            children = new Trie.Node[26];
+            isLast = false;
+        }
+    }
+
 }
